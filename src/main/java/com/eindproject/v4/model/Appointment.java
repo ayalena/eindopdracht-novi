afa83b5e13ab4cb82aa99573d79231dffad6b14b
ayalena <82506563+ayalena@users.noreply.github.com>
2021-10-28 12:52:47 +0200

    connected database to appointment class

--- src/main/java/com/eindproject/v4/model/Appointment.java
+++ src/main/java/com/eindproject/v4/model/Appointment.java
@@ -1,10 +1,19 @@
 package com.eindproject.v4.model;
 
+import javax.persistence.*;
+
+@Entity(name = "appointments")
 public class Appointment {
 
     //attributes
-    public Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+
+    @Column(length = 80)
     public String date;
+
+    @Column(length = 6)
     public String time;
 
     //getters and setters
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+--INSERT INTO appointments
+--(id, date, time)
+--
+--VALUES
+--(1, "October 24th", "10.00"),
+--(2, "October 24th", "11.00");
+
+      
