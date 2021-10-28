12ad3db568ec2e6ba98a0a74f9975a253a7e9a1e
ayalena <82506563+ayalena@users.noreply.github.com>
2021-10-28 10:56:30 +0200

    exception controller and package to catch errors

--- src/main/java/com/eindproject/v4/controllers/AppointmentsController.java
+++ src/main/java/com/eindproject/v4/controllers/AppointmentsController.java
@@ -1,5 +1,6 @@
 package com.eindproject.v4.controllers;
 
+import com.eindproject.v4.exceptions.RecordNotFoundException;
 import com.eindproject.v4.model.Appointment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +23,7 @@ public class AppointmentsController {
         try {
             return ResponseEntity.ok(appointments.get(id));
         } catch (Exception ex) {
-            return ResponseEntity.badRequest().body("Appointment does not exist");
+            throw new RecordNotFoundException();
         }
     }
 
@@ -34,32 +35,40 @@ public class AppointmentsController {
 
     @DeleteMapping("/appointments/{id}")
     public ResponseEntity<Object> deleteAppointment(@PathVariable int id) {
-        appointments.remove(id);
-        return ResponseEntity.ok("Appointment deleted");
+        try {
+            appointments.remove(id);
+            return ResponseEntity.ok("Appointment deleted");
+        } catch (Exception ex) {
+            throw new RecordNotFoundException();
+        }
     }
 
     @PutMapping("/appointments/{id}")
     public ResponseEntity<Object> updateAppointment(@PathVariable int id, @RequestBody String appointment) {
-        appointments.set(id, appointment);
-        return ResponseEntity.ok("Appointment X updated");
+        try {
+            appointments.set(id, appointment);
+            return ResponseEntity.ok("Appointment X updated");
+        } catch (Exception ex) {
+            throw new RecordNotFoundException();
+        }
     }
 
-    @PatchMapping("/appointments/{id}")
-    public ResponseEntity<Object> modifyAppointmentByDate(@PathVariable int id, @RequestBody String date) {
-        appointments.set(id, date);
-        return ResponseEntity.ok("Appointment X updated to new date");
-    }
-
-    @PatchMapping("/books/{id}")
-    public ResponseEntity<Object> modifyAppointmentByTime(@PathVariable int id, @RequestBody String time) {
-        appointments.set(id, time);
-        return ResponseEntity.ok("Appointment X updated to new time");
-    }
-
-    @GetMapping("/appointments?date={date}")
-    public ResponseEntity<Object> getAllAppointmentsByDate(@RequestParam String appointment, @RequestBody String date) {
-        return ResponseEntity.ok(appointments);
-//        return ResponseEntity.ok("All Appointments for date X");
-    }
+//    @PatchMapping("/appointments/{id}")
+//    public ResponseEntity<Object> modifyAppointmentByDate(@PathVariable int id, @RequestBody String date) {
+//        appointments.set(id, date);
+//        return ResponseEntity.ok("Appointment X updated to new date");
+//    }
+//
+//    @PatchMapping("/books/{id}")
+//    public ResponseEntity<Object> modifyAppointmentByTime(@PathVariable int id, @RequestBody String time) {
+//        appointments.set(id, time);
+//        return ResponseEntity.ok("Appointment X updated to new time");
+//    }
+//
+//    @GetMapping("/appointments?date={date}")
+//    public ResponseEntity<Object> getAllAppointmentsByDate(@RequestParam String appointment, @RequestBody String date) {
+//        return ResponseEntity.ok(appointments);
+////        return ResponseEntity.ok("All Appointments for date X");
+//    }
 
 }
